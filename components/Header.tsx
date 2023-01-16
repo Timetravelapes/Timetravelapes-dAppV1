4e6a3f053bc513b375df906cc98c0a2241996979
Timetravelapes <102059377+Timetravelapes@users.noreply.github.com>
2023-01-16 10:16:53 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,13 @@
-import {  useAddress, useMetamask, useDisconnect } from "@thirdweb-dev/react";
 import Link from "next/link";
 import React from "react";
 import styles from "../styles/Home.module.css";
-
+import {
+  ConnectWallet,
+} from "@thirdweb-dev/react";
 
 export default function Header() {
   // Helpful thirdweb hooks to connect and manage the wallet from metamask.
-  const address = useAddress();
-  const connectWithMetamask = useMetamask();
-  const disconnectWallet = useDisconnect();
+  
  
   
   
@@ -19,7 +18,7 @@ export default function Header() {
         <div>
           <Link href="/" passHref role="button">
             <img
-              src={`./homeicon.png`}
+              src={`./Homeicon.png`}
               alt="Logo"
               width={40}
               height={40}
@@ -30,27 +29,10 @@ export default function Header() {
       </div>
       
       <div className={styles.right}>
-        {address ? (
-          <>
-          
-          <a
-              className={styles.mainButton3}
-              onClick={() => disconnectWallet()}
-            >
-              Disconnect
-            </a>
-            <p style={{ marginLeft: 8, marginRight: 8, color: "#ff00f6" }}>|</p>
-            <p style={{ marginLeft: 8, marginRight: 8, color: "#ff00f6" }}>{address.slice(0, 6).concat("...").concat(address.slice(-4))}</p>
-            <img  src="./wallet1.png" height="38" width="38" alt="TTA" />
-          </>
-        ) : (
-          <a
-            className={styles.mainButton3}
-            onClick={() => connectWithMetamask()}
-          >
-            Connect Wallet
-          </a>
-        )}
+      <ConnectWallet
+  accentColor="#ca33fc"
+ 
+/>
       </div>
       
     </div>
